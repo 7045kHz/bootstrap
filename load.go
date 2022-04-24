68fe3927a7c77899591ebd4653fadc121102eccb
rlb <rlb@mail.rsyslab.com>
2022-04-24 16:24:55 -0400

    Changing structure to Service.Accounts.{Name,Specs{}}

--- load.go
+++ load.go
@@ -9,46 +9,63 @@ import (
 	"github.com/mervick/aes-everywhere/go/aes256"
 )
 
-// Account test
-type Account struct {
-	User     string `json:"User"`
-	Summary  string `json:"Summary"`
-	Domain   string `json:"Domain"`
-	Password string `json:"Password"`
-	Server   string `json:"Server"`
-	Port     int    `json:"Port"`
-	Special  string `json:"Special"`
-}
+// Service struct contains a slice of Accounts struct
 type Service struct {
-	Account []Account
+	Accounts []Accounts `json:"Accounts"`
 }
 
-// BootStrapEnv Struct for storing sensitive information in a JSON file
-// instead of hardcoded
-type BootStrapEnv struct {
-	SqlDomain           string `json:"SqlDomain"`
-	SqlUser             string `json:"SqlUser"`
-	SqlPassword         string `json:"SqlPassword"`
-	SQL_SERVER_FQDN     string `json:"SQL_SERVER_FQDN"`
-	SQL_SERVER_DATABASE string `json:"SQL_SERVER_DATABASE"`
-	SQL_SERVER_PORT     string `json:"SQL_SERVER_PORT"`
+// Accounts struct contains a Name value and Specs struct
+type Accounts struct {
+	Name  string `json:"Name"`
+	Specs Specs  `json:"Specs"`
+}
+
+// Specs struct contains key values needed for that account.
+type Specs struct {
+	Domain   string `json:"Domain"`
+	Password string `json:"Password"`
+	Port     int64  `json:"Port"`
+	Server   string `json:"Server"`
+	Special  string `json:"Special"`
+	Summary  string `json:"Summary"`
+	User     string `json:"User"`
 }
 
 // HashEnv Struct for storing sensitive hashed password value in a JSON file
 // instead of hardcoded
 type HashEnv struct {
-	StartHash string `json:"StartHash"`
+	BootHash string `json:"BootHash"`
 }
 
-// DecryptPassword Method decrypts the Sql Password and BindPassword stored in Bootstrap_File
-func (s *BootStrapEnv) DecryptPassword(h *HashEnv) {
-	s.SqlPassword = aes256.Decrypt(s.SqlPassword, h.StartHash)
+// DecryptPasswords Method decrypts the Password for all accounts with a Password
+// string in the Bootstrap_File (*Service) struct
+func (s *Service) DecryptPasswords(h *HashEnv) {
+	for i, _ := range s.Accounts {
+		// Assume reasonable encrypted password length
+		if len(s.Accounts[i].Specs.Password) > 8 {
+			s.Accounts[i].Specs.Password = aes256.Decrypt(s.Accounts[i].Specs.Password, h.BootHash)
+		}
+	}
 
+}
+
+// GetAccount Method takes a string and searches for that string in Service Account records Name entity,
+// then returns a single Accounts struct populated with queried Account.
+func (s *Service) GetAccount(n string) (a Accounts) {
+	for i, _ := range s.Accounts {
+		// Assume reasonable encrypted password length
+		if s.Accounts[i].Name == n {
+			a.Name = s.Accounts[i].Name
+			a.Specs = s.Accounts[i].Specs
+			return a
+		}
+	}
+	return a
 }
 
 // LoadFile Method loads the JSON Bootstrap_File passed as filename, then populates the struct
-// BootStrapEnv via an Unmarshal
-func (s *BootStrapEnv) LoadFile(filename string) error {
+// Service via an Unmarshal
+func (s *Service) LoadFile(filename string) error {
 	_, err := os.Stat(filename)
 	if errors.Is(err, os.ErrNotExist) {
 		return err
